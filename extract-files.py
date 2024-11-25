1be4dc7a3aba6853dab644b832350ff4174fef68
Bruno Martins <bgcngm@gmail.com>
2024-11-25 20:47:06 +0000

    sm8550-common: Resolve missing symbols on libqcodec2_core

--- extract-files.py
+++ extract-files.py
@@ -93,6 +93,9 @@ blob_fixups: blob_fixups_user_type = {
         'android.media.audio.common.types-V2-cpp.so',
         'android.media.audio.common.types-V3-cpp.so'
     ),
+    'vendor/lib64/libqcodec2_core.so': blob_fixup().add_needed(
+        'libcodec2_shim.so'
+    ),
     'vendor/lib64/vendor.libdpmframework.so': blob_fixup().add_needed(
 	'libhidlbase_shim.so'
     ),
