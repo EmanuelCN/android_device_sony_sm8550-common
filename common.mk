a25193019883af21e80a06b64b3bbb20c1c07c89
EmanuelCN <emanuelghub@gmail.com>
2024-11-30 23:57:25 +0000

    sm8550-common: Override cgroups.json & task_profiles.json
    Taken from aosp and moved SF to foreground cpuset

--- common.mk
+++ common.mk
@@ -138,6 +138,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.capabilityconfigstore@1.0.vendor
 
+# Cgroup & task_profiles
+PRODUCT_COPY_FILES += \
+    system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
