64dd5c33addb96b2cd54129c378eff2c3d039c37
Bruno Martins <bgcngm@gmail.com>
2025-04-25 17:36:15 +0100

    sm8550-common: Switch to Touch AIDL
    Change-Id: I92a19fedb9ea482616c2e3d550e30b69d2a3a5c7

--- common.mk
+++ common.mk
@@ -407,7 +407,7 @@ PRODUCT_PACKAGES += \
 
 # Touch
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.sony
+    vendor.lineage.touch-service.sony
 
 # Update engine
 PRODUCT_PACKAGES += \
